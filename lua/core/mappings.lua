3d5fe4351ad77366b8cea8d92150398ef3f17348
Yernaz Kenesbekov <yernazkenesbekov24@gmail.com>
2026-01-21 12:47:05 +0500

    refactor: обновить и улучшить сочетания клавиш
    - Заменены некоторые сочетания клавиш для Telescope для лучшего соответствия.
    - Добавлены новые сочетания для сохранения, копирования и вставки.
    - Закомментирована неиспользуемая конфигурация nvim-emmet.
    - Добавлены точки с запятой для единообразия стиля.

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -4,22 +4,25 @@
 -- vim.keymap.set("n", "gn", ":Neotree dir=./ position=float<CR>", { noremap = true, silent = true })
 -- vim.keymap.set("n", "g<space>", ":Neotree dir=./ position=float<CR>", { noremap = true, silent = true })
 -- vim.keymap.set("n", "G<space>", ":Neotree position=float git_status<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<C-space>", ":Neotree dir=./ position=float<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<C-S-space>", ":Neotree position=float git_status<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<C-b>", ":Neotree dir=./ position=left<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<space>", ":nohlsearch<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-space>", ":Neotree dir=./ position=float<CR>", { noremap = true, silent = true });
+vim.keymap.set("n", "<C-S-space>", ":Neotree position=float git_status<CR>", { noremap = true, silent = true });
+vim.keymap.set("n", "<C-b>", ":Neotree dir=./ position=left<CR>", { noremap = true, silent = true });
+vim.keymap.set("n", "<space>", ":nohlsearch<CR>", { noremap = true, silent = true });
+vim.keymap.set("n", "gs", ":w<CR>", {noremap = true, silent = true});
+vim.keymap.set("n", "gY", "\"+y<CR>", {noremap = true, silent = true});
+vim.keymap.set("n", "gP", "\"+y<CR>", {noremap = true, silent = true});
 
-vim.keymap.set("n", "gto", ":Telescope find_files<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "gtf", ":Telescope grep_string<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "gtf", ":Telescope find_files<CR>", { noremap = true, silent = true });
+vim.keymap.set("n", "gtg", ":Telescope live_grep<CR>", { noremap = true, silent = true });
 vim.keymap.set("n", "gtr", ":Telescope registers<CR>", {noremap = true, silent = true});
 vim.keymap.set("n", "gtm", ":Telescope marks<CR>", {noremap = true, silent = true});
-vim.keymap.set("n", "gtt", ":TodoTelescope<cr>", {noremap = true, silent = true})
+vim.keymap.set("n", "gtt", ":TodoTelescope<cr>", {noremap = true, silent = true});
 
-vim.keymap.set("n", "<C-n>", ":BufferLineCycleNext<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<C-p>", ":BufferLineCyclePrev<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<C-\\>", ":BufferLinePick<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<C-S-\\>", ":BufferLinePickClose<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-n>", ":BufferLineCycleNext<CR>", { noremap = true, silent = true });
+vim.keymap.set("n", "<C-p>", ":BufferLineCyclePrev<CR>", { noremap = true, silent = true });
+vim.keymap.set("n", "<C-\\>", ":BufferLinePick<CR>", { noremap = true, silent = true });
+vim.keymap.set("n", "<C-S-\\>", ":BufferLinePickClose<CR>", { noremap = true, silent = true });
 
-vim.keymap.set("n", "<C-t>", ":TermOpen<CR>", {noremap = true, silent = true})
+vim.keymap.set("n", "<C-t>", ":TermOpen<CR>", {noremap = true, silent = true});
 
-vim.keymap.set({ "n", "v" }, '<leader>xe', require('nvim-emmet').wrap_with_abbreviation)
+-- vim.keymap.set({ "n", "v" }, '<leader>xe', require('nvim-emmet').wrap_with_abbreviation);
